f1535885e98b825d297503e6902017ec5ba59644
Martín Gesualdo <mgesudev@gmail.com>
2023-07-18 11:25:37 -0300

    Comentamos el createUser al conectar

--- db/connect.ts
+++ db/connect.ts
@@ -10,14 +10,14 @@ async function connectDB() {
     await mongoose.connect(process.env.MONGODB_URL)
     console.log("Conexión exitosa con Mongo DB")
     // await resetDB()
-    await createUser({
-      firstname: "Martín",
-      lastname: "Gesualdo",
-      email: "ignaciogesualdo@gmail.com",
-      login_code: "123456",
-      image_url: "https://www.google.com",
-      roles: { admin: true, seller: true },
-    })
+    // await createUser({
+    //   firstname: "Martín",
+    //   lastname: "Gesualdo",
+    //   email: "ignaciogesualdo@gmail.com",
+    //   login_code: "123456",
+    //   image_url: "https://www.google.com",
+    //   roles: { admin: true, seller: true },
+    // })
   } catch (error) {
     console.log("Hubo un error al conectarnos a la BBDD", error)
   }
